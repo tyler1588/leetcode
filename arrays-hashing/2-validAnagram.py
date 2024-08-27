5cfcdd50709900ed2d37c9d4734f2eeede457bce
Tyler Jackson <87100297+tyler1588@users.noreply.github.com>
2024-08-27 08:38:40 -0400

    refactor valid Anagram

--- arrays-hashing/2-validAnagram.py
+++ arrays-hashing/2-validAnagram.py
@@ -1,16 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        sd, st = {}, {}
-
-        if len(s) != len(t):
-            return False
-
-        for i in range(len(s)):
-            sd[s[i]] = sd.get(s[i], 0) + 1
-            st[t[i]] = st.get(t[i], 0) + 1
-
-        for element in sd:
-            if sd.get(element) != st.get(element):
+        dict = {}
+        for c in s:
+            dict[c] = 1 + dict.get(c, 0)
+        for c in t:
+            dict[c] = dict.get(c, 0) - 1
+        for key in dict.keys():
+            if dict[key] != 0:
                 return False
-
         return True
